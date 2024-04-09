1ab1f2deb331be654e42a72b3a3be98c745bf3b6
Srivasu215 <sri.vasu215@gmail.com>
2024-04-09 13:37:49 +0530

    SREENIVAS

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Read from './Users/Read'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Delete from './Users/Delete'
 import Insert from './Users/Insert'
 // import Update from './Users/Update'
 
 const Users = () => {
     return (
-        <>
-            <Read></Read>
-            {/* <Delete></Delete> */}
-            <Insert />
-            {/* <Update></Update> */}
-        </>
+        <Router>
+            <Switch>
+                <Route path="/" component={Insert} />
+                <Route exact path="/insert" component={Read} />
+
+                {/* Add other routes here if needed */}
+            </Switch>
+        </Router>
+
     )
 }
 
--- frontend/src/Users/Read.jsx
+++ frontend/src/Users/Read.jsx
@@ -1,28 +1,51 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Read = () => {
-    const [FetchData, setFetchData] = useState("");
-    console.log("FetchData:",FetchData);
-
+    const [FetchData, setFetchData] = useState([]);
 
     useEffect(() => {
+        const handleSubmit = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/Api/User');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const LocalReturnData = await response.json();
+                setFetchData(LocalReturnData);
+                console.log("LocalReturnData::", LocalReturnData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
         handleSubmit();
     }, []);
 
-
-
-
-    const handleSubmit = async () => {
-
-        const response = await fetch('http://localhost:8000/Api/User');
-        let LocalReturnData = await response.json();
-        setFetchData(LocalReturnData)
-        console.log("LocalReturnData::", LocalReturnData);
-        // Do something with the form data
-    };
-    <>
-
-    </>
+    return (
+        <div>
+            <table style={{ borderCollapse: 'collapse', border: '1px solid black' }}>
+                <thead>
+                    <tr>
+                        <th style={{ border: '1px solid black' }}>firstName</th>
+                        <th style={{ border: '1px solid black' }}>lastName</th>
+                        <th style={{ border: '1px solid black' }}>email</th>
+                        <th style={{ border: '1px solid black' }}>password</th>
+                        <th style={{ border: '1px solid black' }}>gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {FetchData.map((item) => (
+                        <tr key={item._id}>
+                            <td style={{ border: '1px solid black' }}>{item.firstName}</td>
+                            <td style={{ border: '1px solid black' }}>{item.lastName}</td>
+                            <td style={{ border: '1px solid black' }}>{item.email}</td>
+                            <td style={{ border: '1px solid black' }}>{item.password}</td>
+                            <td style={{ border: '1px solid black' }}>{item.gender}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default Read
+export default Read;
